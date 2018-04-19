f3d99ae5a8639d1dc60b7e43db7310a58777383c
dweckerly <d.weckerly@gmail.com>
2018-04-19 11:52:18 -0500

    added cpr functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 var timerCode = null;
 var timerShock = null;
+var timerCPR = null;
+var timerCPRPause = null;
 
 var codeTimer = {
+    'min': 0,
+    'sec': 0
+}
+var cprTimer = {
     'min': 0,
     'sec': 0
 }
@@ -17,11 +23,22 @@ var medTimer = {
 shockCount = 0;
 shock = false;
 
+cprPause = false;
+cprPauseCount = 0;
+
 //shock button click functionality
 $('#shock-btn').click(function() {
     $('#shock-btn-div').fadeOut('fast', function() {
         $('#shock-timer-div').fadeIn("fast", function() {});
     });
+    shockClick();
+});
+
+$('#shock-btn-shock').click(function() {
+    shockClick();
+});
+
+function shockClick() {
     shockCount++;
     $('#shock-badge').html(shockCount);
     clearInterval(timerShock);
@@ -29,7 +46,55 @@ $('#shock-btn').click(function() {
     timerShock = setInterval(function() {
         timerDisplay('shock', shockTimer);
     }, 1000);
+}
+
+$('#cpr-btn').click(function() {
+    $('#cpr-btn-div').fadeOut('fast', function() {
+        $('#cpr-timer-div').fadeIn("fast", function() {});
+    });
+    cprClick();
 });
+
+$('#cpr-btn-cpr').click(function () {
+    if(!cprPause) {
+        $('#cpr-btn-cpr').removeClass('btn-info').addClass('btn-secondary');
+        $('#cpr-btn-cpr').html('<i class="fa fa-play" style="font-size:24px;color:rgb(255, 255, 255);"></i>')
+        cprPauseTimer();
+        cprPause = true;
+    } else {
+        $('#cpr-btn-cpr').removeClass('btn-secondary').addClass('btn-info');
+        $('#cpr-btn-cpr').html('<i class="fa fa-pause" style="font-size:24px;color:rgb(255, 255, 255);"></i>')
+        cprClick();
+        cprPause = false;
+    }
+});
+
+function cprPauseTimer() {
+    clearInterval(timerCPR);
+    $('#cpr-badge').html(cprPauseCount);
+    timerCPRPause = setInterval(function() {
+        cprPauseCount++;
+        $('#cpr-badge').html(cprPauseCount);
+        if(cprPauseCount < 10 && cprPauseCount >= 5) {
+            $('#cpr-container').addClass('pulse-warn');
+        }
+        if(cprPauseCount >= 10) {
+            $('#cpr-container').removeClass('pulse-warn').addClass('pulse-danger');
+        }
+    }, 1000);
+}
+
+function cprClick() {
+    clearInterval(timerCPRPause);
+    $('#cpr-container').removeClass('pulse-warn');
+    $('#cpr-container').removeClass('pulse-danger');
+    clearTimer('cpr', cprTimer);
+    $('#cpr-badge').html('-');
+    cprPauseCount = 0;
+    timerCPR = setInterval(function() {
+        timerDisplay('cpr', cprTimer);
+    }, 1000);
+}
 
 // start button click functionality
 $('#start-btn').click(function() {

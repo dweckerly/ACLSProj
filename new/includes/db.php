480095483b4f8aceab3be6cb854e3c27ddcbd971
David <d.weckerly@gmail.com>
2019-03-19 08:01:20 -0500

    clean up

--- new/includes/db.php
+++
@@ -1,2 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "u407005759_user", "R@n$0m!?!", "u407005759_cm");
--- new/toolbar/deleteToolbar.php
+++
@@ -1,8 +0,0 @@
-<?php
-$id = $_POST['id'];
-include_once("../../includes/db.php");
-$sql = "DELETE FROM toolbarButtons WHERE data='$id";
-mysqli_query($conn, $sql);
-
-mysqli_close();
-header('Location: /');

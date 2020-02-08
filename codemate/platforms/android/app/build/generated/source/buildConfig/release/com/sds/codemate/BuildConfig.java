b794d4e57f0d655c89cb27353f5bbb9aeacd5600
David <d.weckerly@gmail.com>
2020-02-07 21:44:03 -0600

    latest build

--- codemate/platforms/android/app/build/generated/source/buildConfig/release/com/sds/codemate/BuildConfig.java
+++ codemate/platforms/android/app/build/generated/source/buildConfig/release/com/sds/codemate/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.sds.codemate";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 10004;
-  public static final String VERSION_NAME = "1.0.4";
+  public static final int VERSION_CODE = 10005;
+  public static final String VERSION_NAME = "1.0.5";
 }
